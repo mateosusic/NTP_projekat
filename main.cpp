5487d55c1d898927db68bcb0968f29067eb350a9
Mateo Susic <mateosusic@MacBook-Pro-od-Mateo.local>
2023-05-26 10:53:53 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+// Zadatak za Ahmeda,
+
+//void registracija korisnika
+//Napraviti kod za funkciju u kojem osoba unosi svoj ime,korisnicki ID,
+// pocetno stanje novca, u poseban file, e.g. korisnici csv.
+//
+// Dakle treba napraviti struct korisnika i bazu u kojoj se on prijavljuje
+
+
+
+
+// Zadatak za Vildanu
+//
+//Vildana napravi kod koji za sada izvlaci ID iz struct korisnik, kao neki page za prijavu koji ce
+// biti u main programu, takoder ako je moguce
+// neka postoji neki tip koda koji je vezan kao sifra tog korisnika koju samo on zna.
+// Potom upisuje koliko novca on ulaze i koje dionice kupuje. za dionice vidi zadatk za Matea
+
+//Zadtak za mene
+//Napisati cu kod za dionice, tj razne opcije za ulaganje, moram napraviti RTC za mjerenje dobitaka i gubitaka
+//naci neki algoritam za Berzu. Napraviti takoder razne opcije.
+int main(){
+
+
+    return 0;
+}
