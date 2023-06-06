d4356010a81415af171d494409f6ca02bc092da1
vildana <vildana.duric.22@size.ba>
2023-06-06 11:30:31 +0200

    evo mateo pogledaj

---
+++ dio.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// Struktura dionice
+struct Dionica {
+    string puniNaziv;
+    string skracenica;
+    double vrijednost;
+};
+
+// Struktura korisnik
+struct Korisnik {
+    string ime;
+    string prezime;
+    int id;
+    double kapital;
+    vector<Dionica> dionice;
+};
+
+// Funkcija za unos novog korisnika
+Korisnik unosKorisnika() {
+    Korisnik noviKorisnik;
+    cout << "Unesite ime korisnika: ";
+    cin >> noviKorisnik.ime;
+
+    cout << "Unesite prezime korisnika: ";
+    cin >> noviKorisnik.prezime;
+
+    cout << "Unesite ID korisnika: ";
+    cin >> noviKorisnik.id;
+
+    cout << "Unesite pocetni kapital korisnika: ";
+    cin >> noviKorisnik.kapital;
+
+    return noviKorisnik;
+}
+
+int main() {
+    Korisnik korisnik = unosKorisnika();
+    cout << "Dobrodosli, " << korisnik.ime << " " << korisnik.prezime << "!" << endl;
+    cout << "Vas ID korisnika je: " << korisnik.id << endl;
+
+    return 0;
+}
